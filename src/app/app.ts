76395f441b81ff4778e89b45bce2eb054086cc6f
15138730370 <1037483576@qq.com>
2018-01-05 11:23:00 +0800

    first commit

---
+++ src/app/app.ts
@@ -0,0 +1,15 @@
+import { NgModule, ModuleWithProviders } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+    imports: [
+        CommonModule,
+    ],
+    exports: [
+    ],
+    declarations: [
+    ],
+    providers: [
+    ],
+})
+export class SwiperModule { }
